7d7ecc065b806791b88807a4957ac17eb35dceff
ticincloud <scm.feina@gmail.com>
2020-12-16 12:21:10 +0100

    afegir util2 branca1

--- main.py
+++ main.py
@@ -3,15 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import utilitats as ut
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import util2 as ut2
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hola Mon')
-    ut.salutacio()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('Hola sóc en ', ut2.nom)
